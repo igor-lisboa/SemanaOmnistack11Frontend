13549e9e5ede39f9f98dfa20227c456df5f76b38
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2020-03-27 00:43:21 -0300

    incluindo pag d register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import './global.css';
-import Logon from './pages/Logon';
+import Routes from './router';
 
 function App() {
   return (
-    <Logon/>
+    <Routes/>
   );
 }
 
